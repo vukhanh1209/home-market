dbb1c105a292e1ec4c9461115bc7e9904be07179
vukhanh1209 <nguyenhongvukhanh@gmail.com>
2023-05-14 15:40:41 +0700

    update header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,8 +3,9 @@ import {FiUser, FiShoppingBag} from 'react-icons/fi'
 import {CgMenuGridO} from 'react-icons/cg'
 import {BiChevronDown} from 'react-icons/bi'
 import { Link } from "react-router-dom";
-
-
+import {IoMailOutline} from 'react-icons/io5'
+import {HiOutlineUserCircle} from 'react-icons/hi2'
+import {TfiReceipt} from 'react-icons/tfi'
 
 const Header = () => {
     return (
@@ -34,17 +35,17 @@ const Header = () => {
 
                         <div className="flex justify-center text-[#383634]">
                             <div className={`flex items-center h-14 font-bold text-base z-10`}>
-                                <a href="" className="relative group flex items-center h-full px-4 mr-2 rounded-xl hover:text-[#4C7C7D] ">
+                                <a href="" className="relative group flex items-center h-full mx-4 rounded-xl hover:text-[#4C7C7D] ">
                                     <div className="flex justify-center items-center">
                                         <CgMenuGridO className="text-3xl mr-2"/>
                                         <div>
                                             Danh mục
                                         </div>
                                     </div>
+                                    <div className="absolute w-full h-4 bg-transparent top-[100%]"></div>
                                     
                     
-                                    <div className="group-hover:inline-block hidden text-left mt-14" id="ProductDropdown">
-                                        
+                                    <div className="group-hover:inline-block hidden text-left mt-16" id="ProductDropdown">
                                         <div className="absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-[#383634] ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                                             <div className="py-1" role="none">
 
@@ -72,37 +73,42 @@ const Header = () => {
                     </div>
                     
 
-                    <div className="flex items-center justify-center gap-x-10 z-10">
-                        {/* <button className="px-4 py-3 rounded-full text-[#383634] font-semibold text-sm hover:bg-slate-50 " >Đăng ký</button>
+                    <div className="flex items-center justify-center z-10">
+                        {/* <button className="px-4 py-3 rounded-full text-[#383634] font-semibold text-sm hover:bg-primary--dark mr-3" >Đăng ký</button>
                         <button className="px-4 py-3 rounded-full textt-[#fff] font-semibold text-sm bg-[#4C7C7D] hover:opacity-80 ">Đăng nhập</button> */}
 
-                        <div className="group relative hover:cursor-pointer ">
-                            <a href="" className="relative group flex items-center h-full px-4 mr-2 rounded-xl hover:text-[#4C7C7D] ">
+                        <div className="group relative hover:cursor-pointer mr-10">
+                            <a href="" className="relative group flex items-center h-full mx-4 rounded-xl hover:text-[#4C7C7D] ">
                                 <div className="flex justify-center items-center gap-x-2">
                                     <FiUser className="text-2xl text-[#383634]"/>
                                     <span className="text-sm text-slate-600 font-bold whitespace-nowrap">Geralt</span>
                                     <BiChevronDown className="text-lg text-[#383634]"/>
                                 </div>
+
+                                <div className="absolute w-full h-8 bg-transparent top-[100%]"></div>
+
                                 
                 
-                                <div className="group-hover:inline-block hidden text-left mt-14" id="ProductDropdown">
+                                <div className="group-hover:inline-block hidden text-left mt-16 " id="ProductDropdown">
                                     
-                                    <div className="absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-[#383634] ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
-                                        <div className="py-1" role="none">
+                                    <div className="absolute right-0 z-10 mt-2 w-fit origin-top-right divide-y divide-gray-100 rounded-xl bg-white shadow-lg ring-1 ring-[#383634] ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                                        <div className="py-2 font-medium " role="none">
+                                            <li href="#" className="flex items-center gap-x-3 text-primary px-6 py-2 text-sm whitespace-nowrap hover:bg-primary--dark" role="menuitem" tabIndex="-1" id="menu-item-0">
+                                                <HiOutlineUserCircle className="h-6 w-6"/>
+                                                <span> Quản lý tài khoản</span>
+                                            </li>
+                                            <li href="#" className="flex items-center gap-x-3 text-primary px-6 py-2 text-sm whitespace-nowrap hover:bg-primary--dark" role="menuitem" tabIndex="-1" id="menu-item-1">
+                                                <IoMailOutline className="h-6 w-6"/>
+                                                <span>Đơn hàng của tôi</span>
+                                            </li>
+                                            <li href="#" className="flex items-center gap-x-3 text-primary px-6 py-2 text-sm whitespace-nowrap hover:bg-primary--dark" role="menuitem" tabIndex="-1" id="menu-item-1">
+                                                <TfiReceipt className="h-6 w-6"/>
+                                                <span>Đơn mua của tôi</span>
+                                            </li>
 
-                                            <li href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Edit</li>
-                                            <li href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Duplicate</li>
                                         </div>
-                                        <div className="py-1" role="none">
-                                            <li href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">Archive</li>
-                                            <li href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">Move</li>
-                                        </div>
-                                        <div className="py-1" role="none">
-                                            <li href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-4">Share</li>
-                                            <li href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-5">Add to favorites</li>
-                                        </div>
-                                        <div className="py-1" role="none">
-                                            <li href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-6">Delete</li>
+                                        <div className="py-1 hover:bg-primary--dark rounded-b-xl" role="none">
+                                            <li href="#" className="font-medium text-primary block px-4 py-2 text-sm whitespace-nowrap  text-center" role="menuitem" tabIndex="-1" id="menu-item-6">Đăng xuất</li>
                                         </div>
                                     </div>
                                 </div>
