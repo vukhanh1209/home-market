2f6e9d8b32c5ecdfa93f22f35364fd6597338963
vukhanh1209 <nguyenhongvukhanh@gmail.com>
2023-06-09 09:45:31 +0700

    update baseURL in api.js file

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api/"
+  // baseURL: "http://localhost:3001/api/"
+  baseURL: "https://home-market-be-production.up.railway.app/"
 });
 
 export default API;
